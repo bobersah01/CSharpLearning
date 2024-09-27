99dcb094f633d2021e6a561490836458dbc7a3db
Şaban Ömer Şahin <101294124+bobersah01@users.noreply.github.com>
2024-09-28 00:07:45 +0300

    FourthClass LeetCode - 88 - Merge Sorted Array
    I have tried to solve the leetcode 88 Merge Sorted Array question and wanted to published.

---
+++ FourthClass/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace FourthClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Normalde bu değerlerin kullanıcı tarafından girilmesi gerekiyor. En azından arrayler tanımlandıktan sonra, içerlerine eklemeleri loop içerisinde yapılabilir.
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int[] nums2 = { 2, 5, 6};
+            int m = nums1.Length;
+            int n = nums2.Length;
+
+            Console.WriteLine("First Array: ");
+            foreach (int i in nums1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\nSecond Array: ");
+            foreach (int i in nums2)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] nums3 = new int[m + n]; //Toplam Array
+
+            for (int i = 0; i< m;i++)
+            {
+                nums3[i] = nums1[i];
+            }
+
+            int counter = 0;
+            for (int i = m; i<m+n;i++)
+            {
+                nums3[i] = nums2[counter];
+                counter++;
+            }
+
+            int zeroCounter = 0;
+            Console.WriteLine("\nTotal Array: ");
+            foreach (int i in nums3)
+            {
+                Console.WriteLine(i);
+                if (i == 0)
+                {
+                    zeroCounter++;
+                }
+            }
+
+            int[] nonZeroTotalArray = new int[nums3.Length - zeroCounter];
+            for (int i = 0;i< nonZeroTotalArray.Length;i++)
+            {
+                if (nums3[i] != 0)
+                {
+                    nonZeroTotalArray[i] = nums3[i];
+                }
+            }
+
+            Console.WriteLine("\nTotal Array Without Zero: ");
+            foreach (int i in nonZeroTotalArray)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
+
